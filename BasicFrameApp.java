ad29936e584487950150d5fe9d1a8909f9a89040
Ramuluvhana-Eric <140482675+Ramuluvhana-Eric@users.noreply.github.com>
2023-07-25 08:16:40 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicframeapp;
+
+import za.ac.tut.ul.MyFirstFrame;
+
+/**
+ *
+ * @author ericm
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    MyFirstFrame mff;
+    mff = new MyFirstFrame();
+    }
+    
+}
